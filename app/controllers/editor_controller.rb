4bb60e5a7280e5c1d019bd3e9b7185188377bb79
jackieju <jackie.ju@gmail.com>
2010-04-14 05:47:38 +0800

    add wang zhai

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -90,7 +90,25 @@ logger.info("+title:#{search}+uid:#{current_user[:id]}")
       docid = doc[:id].to_s
       redirect_to :action=>'index', :docid=>docid
     end
+    
+    def save_doc (doc)
+      id=doc[:id]
+      content=doc[:content]
+      begin
+        dir = id.to_i/100
+        dir = "/var/oe/docs/#{dir.to_s}"
+        FileUtils.makedirs(dir)
+        logger.info("===========>#{dir}/#{id}<====")
+        aFile = File.new("#{dir}/#{id}","w")
+        aFile.puts content
+        aFile.close
+      rescue Exception=>e
+        logger.error e
+      end
 
+      doc.save!()
+    end
+    
     def save
       title = params[:title]
       content = params[:content]
@@ -105,19 +123,9 @@ logger.info("+title:#{search}+uid:#{current_user[:id]}")
       doc[:title] = title.gsub("\n","").gsub("\r","");
       doc[:content] = content;
 
-  	begin
-      dir = id.to_i/100
-      dir = "/var/oe/docs/#{dir.to_s}"
-      FileUtils.makedirs(dir)
-      logger.info("===========>#{dir}/#{id}<====")
-      aFile = File.new("#{dir}/#{id}","w")
-      aFile.puts content
-      aFile.close
-    rescue Exception=>e
-      logger.error e
-    end
+    save_doc(doc)
 
-    doc.save!()
+ 
 
 =begin
         analyzer = RMMSeg::Ferret::Analyzer.new { |tokenizer|
@@ -185,8 +193,104 @@ end
       redirect_to :action=>'list'
     end
 
+    def esc(str)
+      print "----->str1:#{str}\n"
+        str=str.gsub("&","&amp;")
+        print "----->str2:#{str}\n"
+       str.gsub(/([^ %&=;?:\/a-zA-Z0-9_.-]+)/n) do
+      '%' + $1.unpack('H2' * $1.size).join('%').upcase
+      end.tr(' ', '+')
+    end
 
+    def getPage(url, limit)
+      url = esc(url)
+      print "--------------------------------->url1:#{url}\n"
+      res = Net::HTTP.get_response(URI.parse(url))
+      print "--------------------------------->locate:#{res['location']}\n";
+      #print "--------------------------------->response value:#{res}\n"
+      case  res
+        when Net::HTTPSuccess     then return res
+        when Net::HTTPRedirection 
+            if (limit<=0) 
+              return res 
+            else 
+              getPage(res['location'], limit-1)
+            end
+      end
+      return res
+    end
+    
+    def logit
+      url = params[:url];
+      uri =  URI.split(esc(url));
+      url =~ /.*?:\/\/.*\//m
+      context = $&
+    begin
+      res = getPage(url, 10)
+      print "------->inspect:"+res.inspect+"\n"
+         content = res.body
+      rescue Exception=>e
+        print "=====>exception:#{e}\n"
+        content = "Cannot get page"
+    end
+      # get charset
+      if (content =~/<meta.*? charset=[\'\"]*([-\w\d]+)[\'\"]*.*?>/mi)
+      encode = $1.gsub(" ", "")
+    else
+      encode = "utf-8"
+    end
+      print "---->charset="+encode+"\n"
+      if  not (encode =~ /~utf-8$/i)
+        content =	Iconv.conv('utf-8', encode, content)
+      end
+     # print "------>content_type:"+content.type_params[:charset]+"\n"
+      print "\n"
+      title_re = /<title>(.*)?<\/title>/mi
+      
+      m = title_re.match(content)
+      print "\n---------->title:#{m}"
+      if (m)
+        title = m[1].gsub(/[\/|\\*\(\)?\[\];:'\",\.]/,"")
+      else
+        title = ""
+      end
+         print "\n---------->content0:#{content}"
+      content_re = /<body(.*?)>(.*)<\/body>/xmi
+      m = content_re.match(content)     
+      if m
+         print "\n---------->m:#{m}\n"
+       print "\n---------->m1:#{m[1]}\n"
+        print "\n---------->m2:#{m[2]}\n"
+      content = m[2]
+     end
+      print "\n---------->content1:#{content}"
+     content = content.gsub(/<script(.*?)<\/script>/mi, "")
+     
+     # fix img
+     content = content.gsub(/(<img.*?src=[\"\'])\//){|m| "#{$1}#{uri[0]}:\/\/#{uri[2]}\/"}
+     content = content.gsub(/(<img.*?src=[\"\'])([^\/(http:\/\/)])/){|m| "#{$1}#{context}#{$2}"}
+     #  print "\n---------->content:#{content}"
+     
+     
+      content = "<p style=\"background:\#ccccff\"><a href='#{params[:url]}' >#{params[:url]}</a></p>#{content}";
+      doc = Doc.new({
+      :uid=>current_user[:id],
+      :title=>title,
+      :doctype=>1, 
+      :prop=>'',
+      :tags=>''
+      })
+      doc.save!()
+      @doc = doc
 
+      docid = doc[:id].to_s
+      doc[:content] = content
+      save_doc(doc)
+      redirect_to :action=>'index', :docid=>docid
+
+    end
+    
+    
     def pub_by_mwla
       username = params[:username]
       pwd = params[:pwd]
