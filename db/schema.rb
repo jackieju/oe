190c418934efb78746cab6582f5b3341f9435960
Jackie Ju <jackie.ju@gmail.com>
2010-04-14 14:14:07 +0800

    c

--- db/schema.rb
+++ db/schema.rb
@@ -61,9 +61,9 @@ ActiveRecord::Schema.define(:version => 20100316222609) do
   end
 
   create_table "open_id_authentication_nonces", :force => true do |t|
-    t.integer "timestamp",  :null => false
+    t.integer "timestamp",                  :null => false
     t.string  "server_url"
-    t.string  "salt",       :null => false
+    t.string  "salt",       :default => "", :null => false
   end
 
   create_table "publishes", :force => true do |t|
