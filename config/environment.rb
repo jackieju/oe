aacb95c69e526dd23d66dabad2f88ceeeece5bf3
root <root@AY1405161626354151c3Z.(none)>
2014-10-26 14:59:46 +0800

    c

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -52,7 +52,7 @@ Rails::Initializer.run do |config|
  # config.gem 'datanoise-actionwebservice', :lib => 'actionwebservice', :version => '2.2.2' 
 
 end
-require 'action_web_service' 
+#require 'action_web_service' 
 
  
 #config.gem 'rmmseg'
